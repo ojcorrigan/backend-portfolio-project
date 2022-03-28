2201a2819fa8ffb415f64f449baa3207b74f3245
ojcorrigan <owen.corrigan5@gmail.com>
2022-03-28 16:32:22 +0100

    created model that fetches article by Id avoiding injection

--- models/models.js
+++ models/models.js
@@ -6,4 +6,10 @@ exports.selectTopics = () => {
   });
 };
 
-exports.selectArticleById = () => {};
+exports.selectArticleById = (article_id) => {
+  return db
+    .query('SELECT * FROM articles WHERE article_id = $1', [article_id])
+    .then((result) => {
+      return result.rows[0];
+    });
+};
