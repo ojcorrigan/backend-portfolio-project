9c44bd064a86a8a8234192ce69174abfb44ef364
ojcorrigan <owen.corrigan5@gmail.com>
2022-03-29 11:51:02 +0100

    cleaned up console.log

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -4,7 +4,6 @@ const { selectUsers } = require('../models/users-models');
 exports.getUsers = (req, res, next) => {
   selectUsers()
     .then((result) => {
-      console.log(result);
       res.status(200).send(result);
     })
     .catch(next);
