42e185e76d609bb55bc46d57adeaf376a510ad57
ojcorrigan <owen.corrigan5@gmail.com>
2022-03-29 16:29:40 +0100

    conflicts resolved

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -21,9 +21,7 @@ exports.getArticles = (req,res,next) => {
     .catch((err) => {
       next(err);
     });
-
 }
-
 
 exports.patchArticleById = (req, res, next) => {
   const{ article_id } = req.params
