b261d0a9df37f8876b9c2729766215b1153646a1
ojcorrigan <owen.corrigan5@gmail.com>
2022-03-31 15:17:36 +0100

    added config to new Pool() in connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-const config =
-  ENV === 'production'
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {};
 
 
 require('dotenv').config({
@@ -20,4 +11,14 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+module.exports = new Pool(config);
