6cbe34593d8307f70cb1338f05f0dd793238246e
ojcorrigan <owen.corrigan5@gmail.com>
2022-04-19 10:33:52 +0100

    added cors to app.js

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 //Routing
-const apiRouter = require('./routes/api-router');
+const apiRouter = require("./routes/api-router");
 
-const { invalidPath } = require('./controllers/misc-controllers');
+const { invalidPath } = require("./controllers/misc-controllers");
 
 const {
   customError,
   psqlError,
   serverError,
-} = require('./controllers/error-controllers');
+} = require("./controllers/error-controllers");
 
 app.use(express.json());
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.all('*', invalidPath);
+app.all("*", invalidPath);
 
 app.use(customError);
 
@@ -25,6 +26,6 @@ app.use(serverError);
 
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(500).send({ msg: 'internal server error' });
+  res.status(500).send({ msg: "internal server error" });
 });
 module.exports = app;
